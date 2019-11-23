2f6f574af7d60c1788a160de584e661a2ccda069
ltropin <ltropin@mail.ru>
2019-11-23 12:21:35 +0500

    Init SportCenter BlazorApp

---
+++ SportCenter/Data/SportContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportCenter.Data
+{
+    public class SportContext : IdentityDbContext
+    {
+        public SportContext(DbContextOptions<SportContext> options)
+            : base(options)
+        {
+        }
+    }
+}
