3d133fbf26631cd1b04173f065f18a54690088ee
Kirill.Manov <35425709+Jeobaz@users.noreply.github.com>
2019-12-01 13:35:51 +0500

    Layout for the Account's views; CSS and JS files

---
+++ SportCenter/wwwroot/lib/bootstrap/dist/js/forms.js
@@ -0,0 +1,61 @@
+﻿(($) => {
+
+    class Toggle {
+
+        constructor(element, options) {
+
+            this.defaults = {
+                icon: 'fa-eye'
+            };
+
+            this.options = this.assignOptions(options);
+
+            this.$element = element;
+            this.$button = $(`<a class="btn-toggle-pass pr-3"><i class="fas ${this.options.icon}"></i></a>`);
+
+            this.init();
+        };
+
+        assignOptions(options) {
+
+            return $.extend({}, this.defaults, options);
+        }
+
+        init() {
+
+            this._appendButton();
+            this.bindEvents();
+        }
+
+        _appendButton() {
+            this.$element.before(this.$button);
+        }
+
+        bindEvents() {
+
+            this.$button.on('click touchstart', this.handleClick.bind(this));
+        }
+
+        handleClick() {
+
+            let type = this.$element.attr('type');
+
+            type = type === 'password' ? 'text' : 'password';
+
+            this.$element.attr('type', type);
+            this.$button.toggleClass('active');
+        }
+    }
+
+    $.fn.togglePassword = function (options) {
+        return this.each(function () {
+            new Toggle($(this), options);
+        });
+    }
+
+})(jQuery);
+
+$(document).ready(function () {
+    $('#password').togglePassword();
+    $('#password2').togglePassword();
+})
